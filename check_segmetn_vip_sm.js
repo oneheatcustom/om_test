7c17f885bb1fce049270132fb2cf6ae437d24def
b_growe <130042961+oneheatcustom@users.noreply.github.com>
2025-05-06 23:30:35 +0300

    Create check_segmetn_vip_sm.js

---
+++ check_segmetn_vip_sm.js
@@ -0,0 +1,8 @@
+(function (){
+if(typeof window._smartico !== "undefined" && typeof window._smartico.init !== "undefined"){
+    window._smartico.api.checkSegmentMatch(20649).then(function (result) {
+        if(result == true){
+        window._smartico.setSkin('v3_growe_india_15');
+        localStorage.setItem("smartico_skin_v3", true);
+        return result}
+});}})();
