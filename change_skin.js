6982bd022d92e053845978eee6e011604ef470ea
b_growe <130042961+oneheatcustom@users.noreply.github.com>
2025-05-06 18:53:50 +0300

    Create change_skin.js

---
+++ change_skin.js
@@ -0,0 +1,9 @@
+<script>
+(function (){
+window._smartico.api.checkSegmentMatch(20649).then(function (result) {
+        if(result == true && typeof window._smartico !== "undefined" && typeof window._smartico.init !== "undefined"){
+        window._smartico.setSkin('v3_growe_india_15');
+        localStorage.setItem("smartico_skin_v3", true);
+        return result}
+});})();
+</script>
