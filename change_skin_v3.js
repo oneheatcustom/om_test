b9f6a04fe5a98cba36b8636950a17a55ded18237
b_growe <130042961+oneheatcustom@users.noreply.github.com>
2025-05-06 22:31:48 +0300

    Update and rename change_skin_v2.js to change_skin_v3.js

---
+++ change_skin_v3.js
@@ -0,0 +1,23 @@
+<script>
+    const accountNumber = window._growthbookRenderer.context.attributes.accountNumber || null;
+	const language = "EN" || null;
+
+	window._smartico_user_id = accountNumber;
+	window._smartico_language = accountNumber && language;
+
+	(function (d, r, b, h, s) {
+		h = d.getElementsByTagName('head')[0];
+		s = d.createElement('script');
+		s.onload = b;
+		s.src = r;
+		h.appendChild(s);
+	})
+	(document, 'https://libs.smartico.ai/smartico.js', () => {
+		_smartico.init("b9c2f276-be35-4b8c-873a-3237486db424-4", { brand_key: "16b5e781" });
+		_smartico.on('props_change', (props) => {
+			    if (props.ach_level_current === 15) {
+                _smartico.setSkin('v3_growe_india_15');
+                }
+		});
+	});
+</script>
