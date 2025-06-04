37612c1841009276c6d0ba76b68dba52b0e798f0
b_growe <130042961+oneheatcustom@users.noreply.github.com>
2025-06-04 17:45:30 +0300

    Update change_skin_v5.js

--- change_skin_v5.js
+++ change_skin_v5.js
@@ -7,6 +7,7 @@
 	})
 	(document, 'https://libs.smartico.ai/smartico.js', () => {
 		_smartico.init("b9c2f276-be35-4b8c-873a-3237486db424-4", { brand_key: "16b5e781" });
+                console.log("smartikoinit");
 		_smartico.on('props_change', (props) => {
 			    if (props.ach_level_current === 15) {
                 _smartico.setSkin('v3_growe_india_15');
